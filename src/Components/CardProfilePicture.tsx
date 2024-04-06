ae011f7c787ec001727225fcec8fe032f8758970
ricardocollado <ricardocolladorothschild@gmail.com>
2024-04-06 11:30:04 -0400

    modifying card stylish

--- src/Components/CardProfilePicture.tsx
+++ src/Components/CardProfilePicture.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import '../styles/CardProfilePicture.css';
 export const CardProfilePicture:React.FC = ()=>{
 
     return(
         <>
+        <div className="profile-pic-container">
+            <img src="" alt="Profile_Pic"/>
+        </div>
         </>
     );
 }
--- src/Components/CardSubContainer.tsx
+++ src/Components/CardSubContainer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/CardSubContainer.css';
+import { CardProfilePicture } from './CardProfilePicture';
 interface Props{
     username:string;
     user:string;
@@ -10,11 +11,15 @@ export const CardSubContainer:React.FC<Props> = ({username, user, fecha, twitt}:
 
     return(
         <div className='card-sub-container'>
+            <CardProfilePicture/>
            <div className="user-data-info">
+            <div className="user-date-name-information-container">
             <p className="user-data-info-paragraph">{`${username}`}</p>
-           <p className="user-date-container">{`${user} ${fecha}`}</p>
+            <p className="user-date-container">{`${user} ${fecha}`}</p>
+            </div>            
+           <p className="twitt-text-container">{`${twitt}`}</p>
            </div> 
-            <p>{`${twitt}`}</p>
+            
         </div>
     );
 }
